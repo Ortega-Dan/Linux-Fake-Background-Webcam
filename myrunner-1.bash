dc3126adf85f3b1df87166c9c5ecab897def089d
Dan Ortega <ortega.dan2010@gmail.com>
2022-03-14 12:36:10 -0500

    Improving launcher logic

--- myrunner-1.bash
+++ myrunner-1.bash
@@ -2,9 +2,12 @@
 
 # only for usage at Dan Ortega's environments (adjust if interested)
 
-sudo modprobe v4l2loopback devices=1 exclusive_caps=1 card_label="fake-cam"
-
 camera=$(python3 myrunner-2.py)
+
+if [ $camera == "MISSINGFAKE" ]; then
+    sudo modprobe v4l2loopback devices=1 exclusive_caps=1 card_label="fake-cam"
+    camera=$(python3 myrunner-2.py)
+fi
 
 if [ $camera == "NONE" ]; then
     echo "Unable to find camera 4. Make sure it is plugged in"
--- myrunner-2.py
+++ myrunner-2.py
@@ -1,17 +1,20 @@
 # only for usage at Dan Ortega's environments (adjust if interested)
 
 import os
+
 stream = os.popen('v4l2-ctl --list-devices')
 
+line = stream.readline()
+if not line.__contains__("fake-cam"):
+    print("MISSINGFAKE")
+    exit()
 
-line = ""
-countLines = 0
+countLines = 1
 camera = "NONE"
 
 while True:
-    line = stream.readline()
     countLines += 1
-
+    line = stream.readline()
     if line.__contains__("Video Capture 4"):
         camera = stream.readline().strip()
         break
